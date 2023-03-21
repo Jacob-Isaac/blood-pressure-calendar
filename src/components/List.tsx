de5ff9ede22e9576773a05c16808cca4852e8b81
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2023-03-21 21:34:26 +0100

    list styling added

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,34 +1,46 @@
-import React from "react"; 
+import React from "react";
 import { useSelector } from "react-redux/es/exports";
-import { selectTasks} from "./Slice";
+import { selectTasks } from "./Slice";
 import { nanoid } from "@reduxjs/toolkit";
 import { Task } from "./interfaces";
 
-  const List = () => {
-    const pressureList: Task[] = useSelector(selectTasks);
-const renderList = (): JSX.Element[] => {
+const List = () => {
+  const pressureList: Task[] = useSelector(selectTasks);
+  const renderList = (): JSX.Element[] => {
     return pressureList.map((person) => {
-        const key = nanoid();
-        return (
+      const key = nanoid();
+      return (
         <li key={key} className="List">
-            <div className="List-header">
-                <h2>{person.up}</h2><h2>/</h2>
-                <h2>{person.down}</h2>
+          <div className="List-order">
+            <div className="List-title">
+            🩸 Blood Pressure
             </div>
-            <p>{person.pulse}</p>
-            <p className="List-note">{person.note}</p>
+            <div className="List-pressScore">
+              <div className="List-upperText">Highest</div>
+              <div className="List-middleText">{person.up}</div>
+              <div className="List-bottomText">mmHg</div>
+            </div>
+            <div>/</div>
+            <div className="List-pressScore">
+              <div className="List-upperText">Lowest</div>
+              <div className="List-middleText">{person.down}</div>
+              <div className="List-bottomText">mmHg</div>
+            </div>
+            <div className="List-pulseScore">
+              <div className="List-upperPulseText">Pulse</div>
+              <div className="wrapper"><div>❤️</div>
+                <div className="List-middleText">{person.pulse}</div></div>
+              <div className="List-bottomPulseText">BPM</div>
+            </div>
+          </div>
+          <div className="List-img"></div>
+          {/* {person.note} */}
         </li>
-        );
+      );
     });
+  };
+
+  return <ul>{renderList()}</ul>;
 };
-
-return (
-    <ul>
-    {renderList()}
-    </ul>
-    );
-
-};
-
 
 export default List;
