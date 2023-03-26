58e5757b68ee93922df8ea82638c0de5222bf801
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2023-03-27 00:19:42 +0200

    style changes and autofocus

--- src/components/AddToList.tsx
+++ src/components/AddToList.tsx
@@ -1,86 +1,96 @@
-import React, {useState} from "react";    
-import { useDispatch } from "react-redux/es/exports"; 
+import React, { useState } from "react";
+import { useDispatch } from "react-redux/es/exports";
 import { addTask } from "./Slice";
 import { nanoid } from "@reduxjs/toolkit";
 
-
 const AddToList = () => {
-    const [input, setInput] = useState({
-        id: undefined,
-        up: undefined,
-        down: undefined,
-        pulse: undefined,
-        note: "",
-    })
-    const dispatch = useDispatch();
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>  {
-setInput({
-    ...input,
-    [e.target.name]: e.target.value
-})
-    }
+  const [input, setInput] = useState({
+    id: undefined,
+    up: undefined,
+    down: undefined,
+    pulse: undefined,
+    note: "",
+  });
+  const dispatch = useDispatch();
+  const focus = React.useRef<HTMLInputElement>(null);
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleClick = (): void =>  {
-       if (
-        !input.up || !input.down || !input.pulse
-       ){
-        return
-       }
-dispatch(
+  const handleClick = (): void => {
+    if (!input.up || !input.down || !input.pulse) {
+      return;
+    }
+    dispatch(
       addTask({
         id: nanoid(),
         up: input.up,
         down: input.down,
         pulse: input.pulse,
-       note: input.note,
+        note: input.note,
       })
     );
-
-       setInput({
-        id: undefined,
-        up: undefined,
-        down: undefined,
-        pulse: undefined,
-        note: "",
-       })
-            }
-        
-
-    return (
-        <div className="AddToList">
-            <input type="number"
-            placeholder="Up"
-            className="AddToList-input"
-            value={input.up}
-            onChange={handleChange}
-            name="up"
-             />
-              <input type="number"
-            placeholder="Down"
-            className="AddToList-input"
-            value={input.down}
-            onChange={handleChange}
-            name="down"
-             />
-                 <input type="number"
-            placeholder="Pulse"
-            className="AddToList-input"
-            value={input.pulse}
-            onChange={handleChange}
-            name="pulse"
-             />
-              <textarea
-            placeholder="Note"
-            className="AddToList-input"
-            onChange={handleChange}
-            name="note"
-             />
-             <button
-             className="AddToList-btn"
-             onClick={handleClick}
-             >Add to List </button>
-        </div>
-    )
+    setInput({
+      id: undefined,
+      up: undefined,
+      down: undefined,
+      pulse: undefined,
+      note: "",
+    });
+    if (focus.current) {
+      focus.current.focus();
     }
+  };
+
+  return (
+    <div className="AddToList">
+      <input
+        type="number"
+        min="90"
+        max="250"
+        placeholder="Up"
+        className="AddToList-input"
+        value={input.up || ""}
+        onChange={handleChange}
+        name="up"
+        ref={focus}
+      />
+      <input
+        type="number"
+        min="40"
+        max="130"
+        placeholder="Down"
+        className="AddToList-input"
+        value={input.down || ""}
+        onChange={handleChange}
+        name="down"
+      />
+      <input
+        type="number"
+        min="30"
+        max="200"
+        placeholder="Pulse"
+        className="AddToList-input"
+        value={input.pulse || ""}
+        onChange={handleChange}
+        name="pulse"
+      />
+      <textarea
+        placeholder="Note"
+        className="AddToList-input"
+        onChange={handleChange}
+        name="note"
+      />
+      <button className="AddToList-btn" onClick={handleClick}>
+        Add to List{" "}
+      </button>
+    </div>
+  );
+};
 
 export default AddToList;
