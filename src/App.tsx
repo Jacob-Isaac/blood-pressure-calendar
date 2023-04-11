3115cbfbd7c894c7287950707f2ec74bb9b43d09
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2023-04-11 12:26:33 +0200

    App converted to meet router requriements

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,51 @@
-import "./App.css";
-import List from "./components/List";
-import AddToList from "./components/AddToList";
-
+import { HashRouter, Route, Routes } from "react-router-dom";
+import NavigationBar from "./common/Navigation";
+import Home from "./components/Home/Home";
+import {Logo} from "./styled"
+import List from "./components/List/List";
+// import NoResult from "./common/NoResult";
+// import List from "./components/List";
+// import AddToList from "./components/AddToList";
 
 function App() {
-
-
   return (
-    <div className="App">
+    <HashRouter>
+      <nav>
+        <NavigationBar />
       {/* <ThemeProvider theme={lightMode === "yellow" ? yellowTheme :
            orange ? orange :
            red ? red :
            violet ? violet :
            lightMode}></ThemeProvider> */}
-      <h1>My blood pressure control panel</h1>
-      <List/>
-      <AddToList></AddToList>
-    </div>
+      <Logo>BLOOD PRESSURE PANEL</Logo>
+      {/* <List/>
+      <AddToList></AddToList> */}
+     
+        <Routes>
+          {/* <Route
+            path="*"
+            element={
+              <NoResult
+                error404={<h1>404  </h1>}
+                title={`Oops! This page doesn't exist! Please go back to Main Page :)`}
+              />
+            }
+          /> */}
+          {/* <Route path="/" element={<Navigate to="movies" />} />
+          <Route path="/movies" element={<Navigate to="1" />} />
+          <Route path="/people" element={<Navigate to="1" />} /> */}
+           <Route path="/" element={<Home />} />
+           <Route path="/test" element={<List />} />
+          {/* <Route path="/movies/:page" element={<MovieList />} />
+          <Route path="/people/:page" element={<PersonList />} />
+          <Route path="/movieDetails/:id" element={<MovieDetails />} />
+          <Route path="/personDetails/:id" element={<PersonDetails />} />
+          <Route path="/search" element={<SearchResults />} /> */}
+        </Routes>
+      </nav>
+    </HashRouter>
   );
 }
 
 export default App;
+
