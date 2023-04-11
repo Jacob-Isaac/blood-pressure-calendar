9d24c65fcdc351fd806f3735575b27e5d5fc5888
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2023-04-11 12:27:12 +0200

    theme for theme provider added

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,2 +1,25 @@
-//różne themes tutaj
-export {}
+export const theme = {
+    colors: {
+        black: "#000",
+        white: "#ffffff",
+        grey: "#808080",
+        whisper: "#F5F5FA",
+        silver: "#C4C4C4",
+        darkerGrey: "#7E839A",
+        mystic: "#E4E6F0",
+        pattensBlue: "#D6E4FF",
+        scienceBlue: "#0044CC",
+
+    },
+    breakpoint: {
+        maxContentSize: 1368,
+        tabletMax: 968,
+        mobileMax: 650,
+        between: 867,
+    },
+    fontSize: {
+        regular: 400,
+        medium: 500,
+        bold: 600,
+    },
+};
