a448516a01b93f5553294946c8872ce8ca41a46a
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2023-05-16 19:16:52 +0200

    notes about which every tale means

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -10,10 +10,11 @@ const Home = () => {
 
       return (       
     <Div>
-      <Div1>1</Div1>
-      <Div2>2</Div2>
-      <Div3>3</Div3>
-      <Div4>4</Div4>
+      <h1>SUMMARY</h1>
+      <Div1><p>30-day average</p>1</Div1>
+      <Div2><p>30-day highest</p>2</Div2>
+      <Div3><p>30-day lowest</p>3</Div3>
+      <Div4><p>30-day termomether</p>4</Div4>
     </Div>
       );
     };
