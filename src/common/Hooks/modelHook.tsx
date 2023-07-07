569c79d0b2a213991c45777eacc5bf6f32db479d
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2023-07-07 13:26:44 +0200

    new models added to represent 30-day average , and also hook to manage them

---
+++ src/common/Hooks/modelHook.tsx
@@ -0,0 +1,37 @@
+import { Task } from "../interfaces";
+import thermometerFull from "../images/ThermometerFull.gltf";
+import thermGreen from "../images/ThermGreen.gltf";
+import thermLime from "../images/ThermLime.gltf";
+import thermYellow from "../images/ThermYellow.gltf";
+import thermOrange from "../images/ThermOrange.gltf";
+import thermPapaya from "../images/ThermPapaya.gltf";
+import thermRed from "../images/ThermRed.gltf";
+import thermViolet from "../images/ThermViolet.gltf";
+
+const asdfasd = 121;
+
+export const useModelHook = () => {
+  return (person: Task): any => {
+    let color: any = thermometerFull;
+    let position: string = "0.04388671956896403m 1.548502198510652m 0.10592409502714872m";
+    let annotation: string = "1- Your average 30-day pressure is above 190/90/60, you need immediately contact to the doctor!"
+    color =
+      person.up >= 190 || person.down >= 130 || person.pulse >= 150
+        ? thermViolet
+        : person.up >= 170 || person.down >= 120 || person.pulse >= 120
+        ? thermRed
+        : person.up >= 160 || person.down >= 100 || person.pulse >= 100
+        ? thermPapaya
+        : person.up >= 150 || person.down >= 95 || person.pulse >= 90
+        ? thermOrange
+        : person.up >= 140 || person.down >= 89 || person.pulse >= 80
+        ? thermYellow
+        : person.up >= 130 || person.down >= 82 || person.pulse >= 70
+        ? thermLime
+        : person.up >= 120 || person.down >= 75 || person.pulse >= 60
+        ? thermGreen
+        : color;
+    return color;
+  };
+};
+
